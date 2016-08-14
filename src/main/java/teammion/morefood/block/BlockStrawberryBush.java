3d160f048473179a86ad6a2419fe1835a278fd02
Stefan Wimmer <stefanwimmer128@gmail.com>
2016-08-14 14:02:54 +0200

    Right click harvest

--- src/main/java/teammion/morefood/block/BlockStrawberryBush.java
+++ src/main/java/teammion/morefood/block/BlockStrawberryBush.java
@@ -17,11 +17,18 @@
 package teammion.morefood.block;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import teammion.morefood.Items;
 import teammion.mioncore.block.BlockCrops;
+
+import javax.annotation.Nullable;
 
 /**
  * Created on 13.08.16 at 17:50
@@ -35,6 +42,22 @@ public class BlockStrawberryBush extends BlockCrops
         super("strawberry_bush", Items.STRAWBERRY);
     }
     
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
+    {
+        if (isMaxAge(state))
+        {
+            for (ItemStack drop : getDrops(worldIn, pos, state, 0))
+                spawnAsEntity(worldIn, pos, drop);
+            
+            worldIn.setBlockState(pos, withAge(4));
+            
+            return true;
+        }
+        
+        return super.onBlockActivated(worldIn, pos, state, playerIn, hand, heldItem, side, hitX, hitY, hitZ);
+    }
+
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
     {
